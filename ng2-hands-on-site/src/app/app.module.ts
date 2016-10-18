704f9668a56cf974025e52691558040852e8338e
Michael Fazio <mfazio23@gmail.com>
2016-10-18 15:10:16 -0500

    Add Firebase integration

--- ng2-hands-on-site/src/app/app.module.ts
+++ ng2-hands-on-site/src/app/app.module.ts
@@ -10,6 +10,13 @@ import { RouterModule } from "@angular/router";
 import { HouseInfoComponent } from "./components/house-info.component";
 import { AppInfoComponent } from "./components/app-info.component";
 import { TopNavComponent } from "./components/top-nav.component";
+import { AngularFireModule } from "angularfire2";
+
+import * as firebase from 'firebase'; // This is just to make the compiler happy
+
+export const firebaseConfig = {
+    //TODO: <enter config here>
+};
 
 @NgModule({
     declarations: [
@@ -28,7 +35,8 @@ import { TopNavComponent } from "./components/top-nav.component";
             {path: 'app-info', component: AppInfoComponent},
             {path: 'houses', component: HouseListComponent},
             {path: 'info/:id', component: HouseInfoComponent}
-        ])
+        ]),
+        AngularFireModule.initializeApp(firebaseConfig)
     ],
     providers: [HouseService],
     bootstrap: [AppComponent]
--- ng2-hands-on-site/src/app/components/house-list.component.ts
+++ ng2-hands-on-site/src/app/components/house-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { House } from '../types/House'
-import { HouseService } from "../services/house.service";
 import { Router } from "@angular/router";
+import { AngularFire, FirebaseListObservable } from "angularfire2";
 
 @Component({
     selector: 'house-list',
@@ -14,16 +14,16 @@ import { Router } from "@angular/router";
 </div>
 <h4 *ngIf="errorMessage">An error has occurred: {{errorMessage}}</h4>
 <ul>
-    <li class="houses" *ngFor="let house of houses; let id = index;" [ngClass]="getRowClass(id)"
+    <li class="houses" *ngFor="let house of houseObservable | async; let id = index;" [ngClass]="getRowClass(id)"
         [class.not-participating]="!house.isParticipating" (click)="goToHouseInfo(id, house)">
         <div class="house-info house-address">{{house.address}}</div>
         <span class="house-info">{{house.name}}</span>
         <div class="house-info house-icons">
-            <img [class.hidden]="!house.hasFullSizeCandyBars" class="house-icon"
+            <img [class.hidden]="!house.hasFullSizeCandyBars" [class.grayed-out]="!house.isParticipating" class="house-icon"
                  src="https://d30y9cdsu7xlg0.cloudfront.net/png/202529-200.png" title="Has Full Size Candy Bars"/>
-            <img [class.hidden]="!house.hasTealPumpkinProjectItems" class="house-icon"
+            <img [class.hidden]="!house.hasTealPumpkinProjectItems" [class.grayed-out]="!house.isParticipating" class="house-icon"
                  src="http://i.imgur.com/tLZVqAs.png" title="Teal Pumpkin Project"/>
-            <img [class.hidden]="!house.hasAdultTreats" class="house-icon"
+            <img [class.hidden]="!house.hasAdultTreats" [class.grayed-out]="!house.isParticipating" class="house-icon"
                  src="https://d30y9cdsu7xlg0.cloudfront.net/png/48763-200.png" title='Has "Adult" treats'/>
         </div>
 
@@ -36,12 +36,13 @@ export class HouseListComponent implements OnInit {
 
     participatingOnly: boolean;
 
-    houses: House[];
+    //houses: House[];
+    houseObservable: FirebaseListObservable<any[]>;
     houseString: string;
 
     private errorMessage: string;
 
-    constructor(private houseService: HouseService, private router: Router) {
+    constructor(private router: Router, private firebase: AngularFire) {
         this.participatingOnly = false;
     }
 
@@ -50,10 +51,11 @@ export class HouseListComponent implements OnInit {
     }
 
     loadHouses(): void {
-        this.houseService.getHouses().subscribe(
+        this.houseObservable = this.firebase.database.list('ng2-hands-on/houses');
+       /* this.houseService.getHouses().subscribe(
             houses => this.houses = houses,
             error => this.errorMessage = <any>error
-        );
+        );*/
     }
 
     getRowClass(index): string {
